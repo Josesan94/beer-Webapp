4312f82d0246be9c5ee29214eb03772269703783
Josesan94 <josesantillan2@hotmail.com>
2024-08-16 17:17:57 -0300

    integrate routing

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -1,11 +1,22 @@
 
+import React from 'react';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import {ProductDetailPage} from './pages/ProductDetailPage'
+import {ProductListPage} from './pages/ProductListPage'
 import './App.css'
+
 
 function App() {
 
   return (
     <>
- 
+      <Router>
+        <Routes>
+          <Route path="/products" element={<ProductListPage/>}  />
+          <Route path="/product/:productId-:productBrand" element={<ProductDetailPage/>}  />
+        </Routes>
+      </Router>
     </>
   )
 }
