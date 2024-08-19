9bc87490f3efa16564766263c978c173e5279c77
Josesan94 <josesantillan2@hotmail.com>
2024-08-19 13:30:19 -0300

    integrate product detail page

--- ui/src/pages/ProductListPage.jsx
+++ ui/src/pages/ProductListPage.jsx
@@ -1,8 +1,30 @@
-
-
+import React from "react";
+import { Link } from "react-router-dom";
+import { useFetch } from "../hooks/useFetch"
+import { formatBrand } from "../utils/formatBrand";
 
 export default function ProductListPage() {
+
+    const {data: products, isLoading, error} = useFetch('/products')
+
+    console.log(products)
+
+    if (isLoading) return <div>Loading...</div>;
+    if (error) return <div>Error loading products: {error.message}</div>;
+
+
   return (
-    <div>ProductListPage</div>
+    <div>
+      <h1>Product Listing Page</h1>
+      <ul>
+        {products.map(product => (
+        <li key={product.id}>
+            <Link to={`/product/${product.id}-${formatBrand(product.brand)}`}>
+            {product.brand}
+            </Link>
+        </li>
+        ))}
+      </ul>
+    </div>
   )
 }
