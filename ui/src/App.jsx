05cf083b0a259f403e146a83983a255d5ca4a14c
Josesan94 <josesantillan2@hotmail.com>
2024-08-20 12:11:34 -0300

    add themeProvider

--- ui/src/App.jsx
+++ ui/src/App.jsx
@@ -4,20 +4,23 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import ProductDetailPage from './pages/ProductDetailPage';
 import ProductListPage from './pages/ProductListPage'
+import { ThemeProvider } from '@mui/material';
 import './App.css'
+import theme from './theme';
 
 
 function App() {
 
   return (
-    <>
+    <ThemeProvider theme={theme}>
       <Router>
         <Routes>
           <Route path="/products" element={<ProductListPage/>}  />
-          <Route exact path="/product/:productId" element={<ProductDetailPage/>} />          <Route path="*" element={<p>Path not resolved</p>} />
+          <Route  path="/product/:productId" element={<ProductDetailPage/>} /> 
+          <Route path="*" element={<p>Path not resolved</p>} />
         </Routes>
       </Router>
-    </>
+    </ThemeProvider>
   )
 }
 
