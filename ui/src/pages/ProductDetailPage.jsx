35bb8a3b3b8ade441b0b4ff61f548d8d67df6e30
Josesan94 <josesantillan2@hotmail.com>
2024-08-20 12:15:57 -0300

    give styles and interaction to pages

--- ui/src/pages/ProductDetailPage.jsx
+++ ui/src/pages/ProductDetailPage.jsx
@@ -1,59 +1,154 @@
 /* eslint-disable no-unused-vars */
 import React, {useEffect, useState} from "react";
-import { useParams } from "react-router-dom";
+import { useParams, useNavigate } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch";
 import api from "../services/api";
+import styled from 'styled-components';
+import { IconButton, Typography, Button, Stack } from '@mui/material';
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+import MoreVertIcon from '@mui/icons-material/MoreVert';
+import Bag from '/products/bag.png'
+
+const Container = styled.div`
+    background-color: #ffffff;
+    padding: 16px;
+    height: 100vh;
+    display: flex;
+    flex-direction: column;
+`;
+
+const Header = styled.div`
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+    margin-bottom: 16px;
+`;
+
+const ProductImage = styled.img`
+    height:240px;
+    width: 240px;
+    max-width: 100%;
+    margin-bottom: 16px;
+`;
+
+const Price = styled(Typography)`
+  color: #FFA500;
+  font-weight: bold;
+`;
+
 
 export default function ProductDetailPage () {
-
+    const navigate = useNavigate()
     const {productId} = useParams()
-    const [id, brand] = productId.split('-')
     const { data: product, isLoading: productLoading, error: productError } = useFetch(`/product/${productId}`);
-    
-    console.log('Product ID:', productId);
-    console.log('Product Brand:', brand);
 
+    const [selectedSku, setSelectedSku] = useState(null);
     const [skuDetails, setSkuDetails] = useState({});
 
     useEffect(() => {
-        const fetchSkuDetails = async () => {
-            if(product && product.skus) {
-                const newSkusDetails = {};
-
-                for(const sku of product.skus) {
-                    try {
-                        const response = await api.get(`/stock-price/${sku.code}`); 
-                        newSkusDetails[sku.code] = response.data
-                    } catch(error) {
-                        console.error(`Error fetching SKU ${sku.code} details:`, error);
-                    }
-                }
-                setSkuDetails(newSkusDetails);
-            };
+        if (product && product.skus.length > 0) {
+          setSelectedSku(product.skus[0].code); 
         }
+      }, [product]);
+
+
+    useEffect(() => {
+    const fetchSkuDetails = async () => {
+        if(product && product.skus) {
+            const newSkusDetails = {};
+
+            for(const sku of product.skus) {
+                try {
+                    const response = await api.get(`/stock-price/${sku.code}`); 
+                    console.log("response de skus", response.data)
+                    newSkusDetails[sku.code] = response.data
+                } catch(error) {
+                    console.error(`Error fetching SKU ${sku.code} details:`, error);
+                }
+            }
+            setSkuDetails(newSkusDetails);
+        };
+    }
 
         fetchSkuDetails();
-    }, [product])
+    }, [selectedSku])
+
+    const handleSizeChange = (sku) => {
+        setSelectedSku(sku.code);
+      };
+
+    const handlebackToProducts = () => {
+        navigate('/products')
+    }
 
 
-    if (productLoading) return <div>Loading...</div>;
+    if (productLoading || !selectedSku || !skuDetails[selectedSku]) {
+        return <div>Loading...</div>;
+      }
     if (productError) return <div>Error loading product details: {productError.message}</div>;
-    if (!product) return <div>Product not found</div>;
 
     return (
-        <div>
-      <h1>{product.brand}</h1>
-      <p>{product.description}</p>
+    <>
+        <Container>
+            <Header>
+                <IconButton onClick={handlebackToProducts}>
+                    <ArrowBackIcon />
+                </IconButton>
+                <Typography variant="h6">Detail</Typography>
+                <IconButton>
+                    <MoreVertIcon />
+                </IconButton>
+            </Header>
+            <Stack display={'flex'} justifyContent={'center'} alignItems={'center'}>
+            <ProductImage src={product.image} alt={product.brand} />
+            </Stack>
+            
 
-      <h2>Available SKUs:</h2>
-      <ul>
-        {product.skus.map(sku => (
-          <li key={sku.code}>
-            {sku.name}: ${skuDetails[sku.code]?.price / 100} - In Stock: {skuDetails[sku.code]?.stock}
-          </li>
-        ))}
-      </ul>
-    </div>
+            <Stack display={"flex"} direction={'row'} justifyContent={'space-between'}>
+            <Typography variant="h5">{product.brand}</Typography>
+            <Price variant="h5">${(skuDetails[selectedSku].price / 100).toFixed(2)}</Price>
+            </Stack>
+            <Stack mb={5} display={'flex'} justifyContent={"initial"} alignItems={'baseline'}>
+            <Typography variant="body1" color="textSecondary">
+                    Origin: {product.origin} | Stock: {skuDetails[selectedSku].stock}
+                </Typography>
+            </Stack>
+            <Stack mb={5} display={'flex'} alignItems={'baseline'}>
+                <Typography variant="h6" color={"black"}>Description</Typography>
+                <Typography variant="body1" color={"textSecondary"} style={{ marginTop: 8, textAlign:"justify" }}>
+                    {product.information}
+                </Typography>
+            </Stack>
+            <Stack mb={5} display={'flex'} >
+                <Typography variant="h6" mb={2} color={'black'} textAlign={'initial'}>Size</Typography>
+                <Stack display={'flex'} direction={'row'} justifyContent={'space-between'}>
+                    {product?.skus.map((sku) => (
+                    <Button
+                        variant={ "outlined"}
+                        style={{
+                            opacity: selectedSku === sku.code ? "1" : "0.5",
+                            border: "1px solid rgba(150, 150, 150, 1)",
+                            borderRadius: "15.5px",
+                            borderColor: selectedSku === sku.code ? "#FFA500" : "rgba(150, 150, 150, 1)",
+                            color: selectedSku === sku.code ? "#FFA500" : "#000",
+                        }}
+                        key={sku.code}
+                        onClick={() => handleSizeChange(sku)}
+                        >
+                        {sku.name}
+                    </Button>
+                    ))}
+                </Stack>
+            </Stack>
+            <Stack display={'flex'} direction={'row'} justifyContent={'space-between'}>
+            <img src={Bag} height={'54px'} width={'54px'}/>
+            <Button sx={{width:'80%', backgroundColor: "#FFA500", color:"white", borderRadius:'12px', }} variant="contained" >
+                Add to cart
+            </Button>
+            </Stack>
+            
+        </Container>
+    </>
     )
 }
 
--- ui/src/pages/ProductListPage.jsx
+++ ui/src/pages/ProductListPage.jsx
@@ -1,30 +1,38 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import { useFetch } from "../hooks/useFetch"
-import { formatBrand } from "../utils/formatBrand";
+import styled from 'styled-components';
+import { Typography } from '@mui/material';
+import ProductListGrid from "../components/ProductGrid";
+import HeaderBar from "../components/NavBar";
+
+
+const WelcomeText = styled.div`
+  margin-left: 36px;
+  display:flex;
+  flex-direction:column;
+  justify-content:start;
+  align-items:start;
+  color:gray;
+`;
+
 
 export default function ProductListPage() {
 
     const {data: products, isLoading, error} = useFetch('/products')
-
-    console.log(products)
 
     if (isLoading) return <div>Loading...</div>;
     if (error) return <div>Error loading products: {error.message}</div>;
 
 
   return (
-    <div>
-      <h1>Product Listing Page</h1>
-      <ul>
-        {products.map(product => (
-        <li key={product.id}>
-            <Link to={`/product/${product.id}-${formatBrand(product.brand)}`}>
-            {product.brand}
-            </Link>
-        </li>
-        ))}
-      </ul>
-    </div>
+    <>
+      <HeaderBar/>
+      <WelcomeText>
+          <Typography variant="h6" color="main" mb={1}  >Hi Mr. Michael,</Typography>
+          <Typography variant="h1" color="black" mb={4}><strong>Welcome Back!</strong></Typography>
+          <Typography variant="h1" color="black">Our Products</Typography>
+      </WelcomeText>
+      <ProductListGrid products={products}/>
+    </>
   )
 }
